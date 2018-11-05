3d40377b65cb02db783af1c98509ee0086074197
Fatih YASAR <fyasar@luteg.com>
2018-11-05 19:00:11 +0300

    .

--- index.py
+++ index.py
@@ -10,8 +10,7 @@ broker_address="192.168.1.55"
 waterLevelSensorPin = 15  # A1
 grovepi.pinMode(waterLevelSensorPin,"INPUT")
 
-plugsCommandTopic1 = '/actuators/plugs/+/open'
-plugsCommandTopic2 = '/actuators/plugs/+/close'
+plugsCommandTopic = '/actuators/plugs/command'
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -20,7 +19,6 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe(plugsCommandTopic1)
-    client.subscribe(plugsCommandTopic2)
 
 
 # The callback for when a PUBLISH message is received from the server.
@@ -31,15 +29,33 @@ def on_message(client, userdata, msg):
     # print(sensorName+" "+returnState(sensorVoltage))
 
 
+def on_start_plug(mosq, obj, msg):
+    # This callback will only be called for messages with topics that match
+    # /actuators/motors/+/stop
+    cmds =  msg.topic.split('/')
+    motorNumber = int(cmds[3])
+    stopMotor(motorNumber)
+    print("on_stop_motor message : " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+def on_stop_plug(mosq, obj, msg):
+    # This callback will only be called for messages with topics that match
+    # /actuators/motors/+/stop
+    cmds =  msg.topic.split('/')
+    motorNumber = int(cmds[3])
+    stopMotor(motorNumber)
+    print("on_stop_motor message : " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+client.message_callback_add("/actuators/plugs/command/+/start", on_start_plug)
+client.message_callback_add("/actuators/plugs/command/+/stop", on_stop_plug)
+
 
 client.connect(broker_address) #connect to broker
 client.loop_start() #start the loop
-
-
 
 
 while True:
@@ -47,14 +63,14 @@ while True:
 
         temperatureData = temp.readTempSensor()
         json_temperatureData = json.dumps(temperatureData)
-        print 'temp :', json_temperatureData
+        #print 'temp :', json_temperatureData
         client.publish("/sensors/temperature", json_temperatureData) 
 
         time.sleep(0.5)
 
         ecData = ec.readEC()
         json_ecData = json.dumps(ecData)
-        print 'ec :', json_ecData
+        #print 'ec :', json_ecData
         client.publish("/sensors/ec", json_ecData) 
 
         time.sleep(0.5)
@@ -66,7 +82,7 @@ while True:
         waterLevelData['time'] = int(time.time())
 
         json_WaterLevelData = json.dumps(waterLevelData)
-        print 'water level :', json_WaterLevelData
+        #print 'water level :', json_WaterLevelData
         client.publish("/sensors/waterlevel", json_WaterLevelData)         
         time.sleep(3)
 

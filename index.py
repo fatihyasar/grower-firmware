c66615aa9bd4d46ba4c72677047be8a3596a328c
Fatih YASAR <fyasar@luteg.com>
2018-11-05 14:52:38 +0300

    .

--- index.py
+++ index.py
@@ -10,6 +10,9 @@ client = mqtt.Client("P1")
 client.connect(broker_address) #connect to broker
 client.loop_start() #start the loop
 
+waterLevelSensorPin = 15  # A1
+grovepi.pinMode(waterLevelSensorPin,"INPUT")
+
 while True:
     try:
 
@@ -25,6 +28,16 @@ while True:
         print 'ec data :', json_ecData
         client.publish("/sensors/ec", json_ecData) 
 
+
+        waterLevelValue = grovepi.analogRead(waterLevelSensorPin)
+        waterLevelData = {}
+        waterLevelData['read'] = 'success'
+        waterLevelData['sensor'] = waterLevelValue;
+        waterLevelData['time'] = int(time.time())
+
+        json_WaterLevelData = json.dumps(waterLevelData)
+        print 'water level data :', json_WaterLevelData
+        client.publish("/sensors/waterlevel", json_WaterLevelData)         
         time.sleep(3)
 
     except IOError:

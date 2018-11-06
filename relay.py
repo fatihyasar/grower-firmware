d25582302d0d73cb764fb89f28d6925fc2b2a04a
Fatih YASAR <fyasar@luteg.com>
2018-11-06 18:32:51 +0300

    .

--- relay.py
+++ relay.py
@@ -3,18 +3,17 @@ import grovepi
 
 # Connect the Grove Relay to digital port D4
 # SIG,NC,VCC,GND
-relay = 2
 
 grovepi.pinMode(relay,"OUTPUT")
 
 def open(relayNumber):
-    grovepi.digitalWrite(relay,1)
+    grovepi.digitalWrite(relayNumber,1)
     time.sleep(2)
     print ("on")
 
 
 def close(relayNumber):
-    grovepi.digitalWrite(relay,0)
+    grovepi.digitalWrite(relayNumber,0)
     time.sleep(2)
     print ("off")
 
@@ -27,4 +26,4 @@ if "__main__" == __name__:
     for x in range(2, 7):
         open(x)
         close(x)    
-        quit()
+    quit()

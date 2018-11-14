3eb2cef26b1c9155f914ed19f3d5e1b2e56a6403
Fatih YASAR <fyasar@luteg.com>
2018-11-14 21:55:07 +0300

    .

--- m.py
+++ m.py
@@ -3,10 +3,7 @@ import string
 import paho.mqtt.client as mqtt
 import time
 import json
-from sensor import sensor
 import grove_i2c_motor_driver
-
-
 
 ########################
 # Main
@@ -14,12 +11,12 @@ import grove_i2c_motor_driver
 if "__main__" == __name__:
 
         motorController1 = grove_i2c_motor_driver.motor_driver(0x0f)
-        motorController1.MotorDirectionSet(0b1010) 
         motorController1.MotorSpeedSetAB(0,0)
+        motorController1.MotorDirectionSet(0b1010) 
 
-        motorController2 = grove_i2c_motor_driver.motor_driver(0x0a)
-        motorController2.MotorDirectionSet(0b1010)
-        motorController2.MotorSpeedSetAB(100,100)
+        #motorController2 = grove_i2c_motor_driver.motor_driver(0x0e)
+        #motorController2.MotorSpeedSetAB(100,100)
+        #motorController2.MotorDirectionSet(0b1010)
     
         quit()
 

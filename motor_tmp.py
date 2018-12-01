a122ef4e90ed3e46e0f027a485d1a1b80e89299a
Fatih YASAR <fyasar@luteg.com>
2018-12-01 19:39:50 +0300

    .

--- motor_tmp.py
+++ motor_tmp.py
@@ -1,7 +1,7 @@
 import grove_i2c_motor_driver
 import time
 
-m = grove_i2c_motor_driver.motor_driver(address=0x0a)
+m = grove_i2c_motor_driver.motor_driver(address=0x0e)
 # controller2 = grove_i2c_motor_driver.motor_driver(address=0x0a)
 try:
         # You can initialize with a different address too: grove_i2c_motor_driver.motor_driver(address=0x0a)
